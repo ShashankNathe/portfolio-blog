3a1ed374cb30fabdb4147a004b4326faa5247759
ShashankNathe <shashanknathe@gmail.com>
2024-07-29 22:27:54 +0530

    added

--- components/ContactHome.jsx
+++ components/ContactHome.jsx
@@ -4,6 +4,73 @@ const ContactHome = () => {
   return (
     <section className="mb-10">
       <h1 className="mb-8 text-3xl font-medium tracking-tighter">Contact me</h1>
+      <form>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label htmlFor="name" className="block text-sm font-medium leading-6 ">
+                  Name
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      id="name"
+                      name="name"
+                      type="text"
+                      placeholder="janesmith"
+                      autoComplete="name"
+                      className="block flex-1 border-0 bg-transparent py-1.5 px-2 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 ">
+                  Email
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="hello@email.com"
+                      autoComplete="email"
+                      className="block flex-1 border-0 bg-transparent py-1.5 px-2 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label htmlFor="about" className="block text-sm font-medium leading-6 ">
+                  About
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="about"
+                    name="about"
+                    rows={3}
+                    className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    defaultValue={""}
+                  />
+                </div>
+                <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Send Message
+          </button>
+        </div>
+      </form>
     </section>
   );
 };
